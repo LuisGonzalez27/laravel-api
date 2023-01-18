093e7503bb0fc5d265be9cc85b69cf117a6fd932
LuisGonzalez27 <luis27coding@gmail.com>
2023-01-18 21:41:03 +0100

    Scaffolding

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{tittle}}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data(){
+      return {
+        tittle:'Hello Vue'
+      }
+    }
+    
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
