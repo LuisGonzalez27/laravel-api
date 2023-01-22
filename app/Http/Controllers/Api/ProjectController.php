edd893cc3cd73dbcf84eacb29a05879ae07cccf5
LuisGonzalez27 <luis27coding@gmail.com>
2023-01-22 14:51:49 +0100

    add Api/ProjectController

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        // $projects = Project::paginate(4);
+        $projects = Project::with('type','technologies')->paginate(4);
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects,
+        ]);
+    }
+    public function show($slug){
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+        if($project){
+            return response()->json([
+                'success' => true,
+                'results' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => 'Nessun project trovato'
+            ]);
+        }
+    }
+}
